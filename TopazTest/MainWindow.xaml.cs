d8faaab90dc6051004ece0cc94dc66d7b23d2364
bpell <bpell@iu.edu>
2024-01-19 18:44:53 -0500

    Add project files.

---
+++ TopazTest/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using Tenray.Topaz;
+
+namespace TopazTest
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Set a static variable on TerminalScriptCommands so methods there
+            // can call back to this window.
+            TerminalScriptCommands.Window = this;
+        }
+
+        private async void ButtonRunScript_Click(object sender, RoutedEventArgs e)
+        {
+            var engine = new TopazEngine();
+            engine.AddType(typeof(TerminalScriptCommands), "Terminal");
+            await engine.ExecuteScriptAsync(@"
+            for (var i = 0; i < 10; i++)
+            {
+                Terminal.Echo(""say "" + i)
+                await Terminal.Pause(1000)
+            }");
+        }
+    }
+}
---
+++ TopazTest/TerminalScriptCommands.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Threading;
+
+namespace TopazTest
+{
+    public static class TerminalScriptCommands
+    {
+        public static MainWindow Window;
+
+        public static void Echo(string? text)
+        {
+            Window.TextOutput.AppendText($"{text}\r\n");
+        }
+
+        public static void Pause(int milliseconds)
+        {
+            DispatcherFrame df = new DispatcherFrame();
+
+            new Thread(() =>
+            {
+                Thread.Sleep(TimeSpan.FromMilliseconds(milliseconds));
+                df.Continue = false;
+
+            }).Start();
+
+            Dispatcher.PushFrame(df);
+        }
+    }
+}
